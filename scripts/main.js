d88c82fbc9d162cea632a3393d72db2a29d65868
Elia Partin <arenaselia@gmail.com>
2023-05-06 18:56:30 -0400

    added for loop to calculate

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,13 @@
     const calculation = []
 
 
-    //passes the event object to each event object with a number, (event) parameter semanatic 
+    //passes the event object to each event object with a number, (event) parameter semantic 
     //event.target is the entire DOM node, targeting the class number in the entire node list
     //created a for loop so the pushNumber function has the click event added to the number button
     
     function pushNumber(event) {
-        alert(event.target.value);
+        // alert(event.target.value);
+        calculation.push(event.target.value);
     }
     const numButtons = document.querySelectorAll('.number');
     for(let i = 0; i < numButtons.length; i++) {
@@ -16,7 +17,8 @@
     }
 
     function pushOperator(event) {
-        alert(event.target.value);
+        // alert(event.target.value);
+        calculation.push(event.target.value);
     }
 
     const operatorButtons = document.querySelectorAll('.operator, .clear, .plus-minus, .percent, .decimal');
@@ -27,8 +29,26 @@
 
     function calculate(event) {
         alert(event.target.value);
+        //for each element in the array
+        let answer = 0;
+        let input = '';
+        for(let i = 0; i < calculation.length; i++) {
+            let num = calculation[i];
+            if(isNaN(num)){
+                num += ' ';
+                input += ' ';
+            }
+            input += num;
+        }
+        console.log('input is' + input );
+        let newArray = input.split(" ");
+        console.log(newArray);
+        //iterate through the array and do the actual operations
+       
     }
-
+    // console.log(calculation[i])
+    // for(let i = 0; i < ); 
+    // let answer = 0; 
     const calculateButton = document.querySelector('.equal-sign');
         calculateButton.addEventListener("click", calculate);
     
